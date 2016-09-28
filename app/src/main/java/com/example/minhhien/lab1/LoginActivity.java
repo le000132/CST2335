c2fa4832d512766ed3f30b9a525dc14e756c69ee
Frederic Le <le000132@algonquinlive.com>
2016-09-27 22:30:32 -0400

    Added 3 activities for Lab 2

--- app/src/main/java/com/example/minhhien/lab1/LoginActivity.java
+++ app/src/main/java/com/example/minhhien/lab1/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.minhhien.lab1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+    SharedPreferences sharedPref;
+    public String name;
+    Button loginButton;
+    final String filename = "MyFIleName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        sharedPref = getSharedPreferences(filename, Context.MODE_PRIVATE);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +39,52 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        final EditText login =(EditText) findViewById(R.id.loginField);
+
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("DefaultEmail", login.getText().toString());
+                editor.commit();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+        sharedPref.getString("DefaultEmail", "email@domain.com");
+        Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "onDestroy()");
     }
 
 }
